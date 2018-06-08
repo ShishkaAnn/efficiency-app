4c53877cea252602de331bf9ea572841030a8773
ShishkaAnn <39994146+ShishkaAnn@users.noreply.github.com>
2018-06-08 08:54:46 +0300

    Add files via upload

--- connection.py
+++ connection.py
@@ -3,16 +3,12 @@ import sqlalchemy
 
 # use this function to get a database engine
 def GetEngine():
-    USER = 'YOUR_APIS_USER'
-    PW = 'YOUR_APIS_PW'
-    URL = '172.21.180.45:35432/ZEUS_RO'
-    url = 'postgresql://{}:{}@{}'.format(USER,PW,URL)
+    url = 'mssql+pymssql://PCSRo01:GrKs0345@172.21.219.136:1433/PCS_RU'
     return sqlalchemy.create_engine(url)
 
 # use this function to run a query
 def run_query(qry):
     return pd.read_sql(sql=qry, con=GetEngine())
-
 
 
 
